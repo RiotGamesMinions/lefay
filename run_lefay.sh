f04661f7f3d756e5f32cf9ba91c4b15f76cfae61
Michael Ivey <michael.ivey@riotgames.com>
2014-01-06 17:11:31 -0800

    hipchat

--- run_lefay.sh
+++ run_lefay.sh
@@ -13,13 +13,13 @@ do
         echo "Stopping! Because you told me to."
         break
     fi
-    git pull origin master
+    git pull origin hipchat
     npm install
-    echo "getting private scripts"
-    (cd ../lefay-private-scripts ; git pull)
-    cp ../lefay-private-scripts/scripts/* scripts/
+    # echo "getting private scripts"
+    # (cd ../lefay-private-scripts ; git pull)
+    # cp ../lefay-private-scripts/scripts/* scripts/
     echo "running lefay!"
-    bin/hubot -a xmpp -n lefay
+    bin/hubot -a hipchat
     echo -n "lefay stopped. waiting 20s before restart. pid is "
     echo $$
     sleep 20
